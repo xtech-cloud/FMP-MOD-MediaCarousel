2446189c8777e32fd428db0a0f04f338b760d3c8
Niall Liu <easlee@outlook.com>
2023-04-11 15:40:33 +0800

    support ImageSlide

--- unity2021/MediaCarousel/Assets/Scripts/Module/MyConfig.cs
+++ unity2021/MediaCarousel/Assets/Scripts/Module/MyConfig.cs
@@ -8,10 +8,29 @@ namespace XTC.FMP.MOD.MediaCarousel.LIB.Unity
     /// </summary>
     public class MyConfig : MyConfigBase
     {
+        public class ImageSlide
+        {
+            [XmlElement("OnOpenedSubjectS")]
+            public Subject[] onOpenedSubjectS { get; set; } = new Subject[0];
+        }
+
+        public class ClickArea
+        {
+            [XmlAttribute("visible")]
+            public bool visible { get; set; } = true;
+
+            [XmlArray("OnClickSubjectS"), XmlArrayItem("Subject")]
+            public Subject[] onClickSubjectS { get; set; } = new Subject[0];
+        }
+
         public class Style
         {
             [XmlAttribute("name")]
             public string name { get; set; } = "";
+            [XmlElement("ClickArea")]
+            public ClickArea clickArea { get; set; }
+            [XmlElement("ImageSlide")]
+            public ImageSlide imageSlide { get; set; } = new ImageSlide();
         }
 
 
--- unity2021/MediaCarousel/Assets/Scripts/Module/MyInstance.cs
+++ unity2021/MediaCarousel/Assets/Scripts/Module/MyInstance.cs
@@ -7,6 +7,9 @@ using UnityEngine.UI;
 using LibMVCS = XTC.FMP.LIB.MVCS;
 using XTC.FMP.MOD.MediaCarousel.LIB.Proto;
 using XTC.FMP.MOD.MediaCarousel.LIB.MVCS;
+using Newtonsoft.Json;
+using System.Text;
+using System.Collections;
 
 namespace XTC.FMP.MOD.MediaCarousel.LIB.Unity
 {
@@ -15,6 +18,20 @@ namespace XTC.FMP.MOD.MediaCarousel.LIB.Unity
     /// </summary>
     public class MyInstance : MyInstanceBase
     {
+        private int currentSlideIndex_ = 0;
+        private ContentReader contentReader_ = null;
+
+        public class Slide
+        {
+            public int index;
+            public string source;
+            public int duration;
+        }
+
+        private List<Slide> slideS_ = new List<Slide>();
+        private float timer_ = 0;
+
+        private Coroutine coroutineTick_ = null;
 
         public MyInstance(string _uid, string _style, MyConfig _config, MyCatalog _catalog, LibMVCS.Logger _logger, Dictionary<string, LibMVCS.Any> _settings, MyEntryBase _entry, MonoBehaviour _mono, GameObject _rootAttachments)
             : base(_uid, _style, _config, _catalog, _logger, _settings, _entry, _mono, _rootAttachments)
@@ -29,6 +46,25 @@ namespace XTC.FMP.MOD.MediaCarousel.LIB.Unity
         /// </remarks>
         public void HandleCreated()
         {
+            if (null == style_)
+            {
+                logger_.Error("style not found");
+                return;
+            }
+
+            var btnClickArea = rootUI.transform.Find("ClickArea").GetComponent<Button>();
+            btnClickArea.gameObject.SetActive(false);
+            if (null != style_.clickArea)
+            {
+                btnClickArea.gameObject.SetActive(true);
+                btnClickArea.onClick.AddListener(() =>
+                {
+                    Dictionary<string, object> variableS = new Dictionary<string, object>();
+                    publishSubjects(style_.clickArea.onClickSubjectS, variableS);
+                });
+            }
+
+
         }
 
         /// <summary>
@@ -46,8 +82,58 @@ namespace XTC.FMP.MOD.MediaCarousel.LIB.Unity
         /// </remarks>
         public void HandleOpened(string _source, string _uri)
         {
+            contentReader_ = new ContentReader(contentObjectsPool);
+            contentReader_.AssetRootPath = settings_["path.assets"].AsString();
+
+
+            int slideIndex = 0;
+            foreach (var section in catalog_.sectionS)
+            {
+                foreach (var content in section.contentS)
+                {
+                    Slide slide = new Slide();
+                    slide.index = slideIndex;
+                    slideIndex += 1;
+                    slideS_.Add(slide);
+                    contentReader_.ContentUri = content;
+                    contentReader_.LoadText("meta.json", (_byte) =>
+                    {
+                        string valueSlide = "";
+                        int valueDuration = 0;
+                        try
+                        {
+                            string json = Encoding.UTF8.GetString(_byte);
+                            ContentMetaSchema schema = JsonConvert.DeserializeObject<ContentMetaSchema>(json);
+                            foreach (var file in schema.AttachmentS)
+                            {
+                                slide.source = string.Format("{0}/_attachments/{1}", schema.foreign_bundle_uuid, file.path);
+                                logger_.Info(slide.source);
+                            }
+                            string strDuration;
+                            if (schema.kvS.TryGetValue("XTC_MediaCarousel/duration", out strDuration))
+                            {
+                                valueDuration = int.Parse(strDuration);
+                            }
+                            schema.kvS.TryGetValue("XTC_MediaCarousel/slide", out valueSlide);
+                        }
+                        catch (System.Exception ex)
+                        {
+                            logger_.Exception(ex);
+                        }
+                        slide.duration = valueDuration;
+                        if (valueSlide == "Image")
+                        {
+                            Dictionary<string, object> openedVariableS = new Dictionary<string, object>();
+                            openedVariableS["{{slide_uuid}}"] = string.Format("XTC_MediaCarousel_{0}_slide_{1}", uid, slide.index);
+                            publishSubjects(style_.imageSlide.onOpenedSubjectS, openedVariableS);
+                        }
+                    }, () => { });
+                }
+            }
+
             rootUI.gameObject.SetActive(true);
             rootWorld.gameObject.SetActive(true);
+            coroutineTick_ = mono_.StartCoroutine(tick());
         }
 
         /// <summary>
@@ -57,6 +143,33 @@ namespace XTC.FMP.MOD.MediaCarousel.LIB.Unity
         {
             rootUI.gameObject.SetActive(false);
             rootWorld.gameObject.SetActive(false);
+            slideS_.Clear();
+            contentReader_ = null;
+            if (null != coroutineTick_)
+            {
+                mono_.StopCoroutine(coroutineTick_);
+                coroutineTick_ = null;
+            }
+        }
+
+        private IEnumerator tick()
+        {
+            currentSlideIndex_ = 0;
+            timer_ = 0;
+            while (true)
+            {
+                yield return new WaitForSeconds(1);
+                timer_ += 1;
+                var slide = slideS_[currentSlideIndex_];
+                if (timer_ > slide.duration)
+                {
+                    currentSlideIndex_ += 1;
+                    if (currentSlideIndex_ >= slideS_.Count)
+                        currentSlideIndex_ = 0;
+                    timer_ = 0;
+                    //publishSubjects();
+                }
+            }
         }
     }
 }
